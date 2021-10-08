a8f38edae50b5785cc70d54e1f5317f8c55bdc3a
Debanshu Datta <debanshudatta123@gmail.com>
2021-10-08 11:56:53 +0530

    Initial commit

---
+++ app/src/main/java/com/debanshu777/snapchatUIComposeClone/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.debanshu777.snapchatUIComposeClone.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val Blue800 = Color(0xFF14213d)
+val Yellow = Color(0xFFFFFC00)
+val Purple = Color(0xFF6900FF)
+val LightBlue = Color(0xFF00CCFF)
+val Grey200 = Color(0xFFe5e5e5)
---
+++ app/src/main/java/com/debanshu777/snapchatUIComposeClone/ui/theme/Theme.kt
@@ -0,0 +1,24 @@
+package com.debanshu777.snapchatUIComposeClone.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColors(
+    primary = Color.White,
+    background = DarkGray,
+    onBackground = Color.White,
+    surface = Yellow,
+    onSurface = DarkGray
+)
+
+@Composable
+fun ComposeSnapChatUITheme(darkTheme: Boolean = true, content: @Composable() () -> Unit) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
