73eb244ef3be20a09224ee7030b01cd618e82cdf
Debanshu Datta <debanshudatta123@gmail.com>
2021-10-23 23:49:33 +0530

    Maps styling and Theming

--- app/src/main/java/com/debanshu777/snapchatUIComposeClone/features/feature_snap_map/SnapMapScreen.kt
+++ app/src/main/java/com/debanshu777/snapchatUIComposeClone/features/feature_snap_map/SnapMapScreen.kt
@@ -1,14 +1,20 @@
 package com.debanshu777.snapchatUIComposeClone.features.feature_snap_map
 
 import android.os.Bundle
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,7 +22,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
+import com.debanshu777.snapchatUIComposeClone.R
+import com.debanshu777.snapchatUIComposeClone.common.utils.ThemeColors
 import com.google.android.libraries.maps.MapView
+import com.google.android.libraries.maps.model.MapStyleOptions
 import com.google.maps.android.ktx.awaitMap
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -29,8 +38,14 @@ fun SnapMapScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .clip(RoundedCornerShape(bottomEnd = 10.dp, bottomStart = 10.dp)),
+            .clip(
+                RoundedCornerShape(
+                    bottomEnd = 10.dp,
+                    bottomStart = 10.dp
+                )
+            ),
     ) {
+        val context = LocalContext.current
         AndroidView(
             {mapView}
         ){
@@ -38,8 +53,25 @@ fun SnapMapScreen() {
             CoroutineScope(Dispatchers.Main).launch {
                 val map= mapView.awaitMap()
                 map.uiSettings.isZoomControlsEnabled= true
+                map.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                        context, R.raw.style_json))
             }
         }
+        Box(modifier = Modifier
+            .rotate(180f)
+            .fillMaxWidth()
+            .height(80.dp)
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                       ThemeColors.MAP_DARK_GRADIENT
+                    ),
+                    startY = 10f
+                ),
+            )
+        )
     }
 }
 
