b356934665e0511b4d521c23e0489eec43d8baeb
Debanshu Datta <debanshudatta123@gmail.com>
2021-12-12 02:06:37 +0530

    Adding Custom fonts

--- app/src/main/java/com/debanshu777/snapchatUIComposeClone/ui/theme/Type.kt
+++ app/src/main/java/com/debanshu777/snapchatUIComposeClone/ui/theme/Type.kt
@@ -2,13 +2,15 @@ package com.debanshu777.snapchatUIComposeClone.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.debanshu777.snapchatUIComposeClone.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    defaultFontFamily = FontFamily.Default,
+    defaultFontFamily = FontFamily(Font(R.font.avenir_next_lt_pro_lt)),
     body1 = TextStyle(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
@@ -17,9 +19,6 @@ val Typography = Typography(
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
-//    topBarHeader = TextStyle(
-//
-//    ),
     button = TextStyle(
         fontWeight = FontWeight.W500,
         fontSize = 14.sp
